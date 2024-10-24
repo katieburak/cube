54ef32905c057c89f02de674efe0d515c6026b27
Katie Burak <katieburak@gmail.com>
2024-10-24 14:14:31 -0700

    added documentation

--- R/cubed.R
+++ R/cubed.R
@@ -1,3 +1,13 @@
+#' Cubes a number or a vector/list of numbers
+#'
+#' @param x a number or a vector/list to be cubed
+#'
+#' @return the cube of x in vector format
+#' @export
+#'
+#' @examples
+#' cubed(3)
+#' cubed(1:3)
 cubed <- function(x){
   purrr::map_dbl(x, function(y) y^3)
 }
